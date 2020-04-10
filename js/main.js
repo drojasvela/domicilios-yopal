55a6cdca5df34453ec6d19eaadb62e6d8b749f02
Diego Rojas <imdiego@imdiego-macbookpro.roam.corp.google.com>
2020-04-09 22:01:00 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var apiKey = "keyieOwATKOFQswlE";
+var urlRestaurants = "https://api.airtable.com/v0/appOPS9QgLsqAcPBI/restaurants?api_key=" + apiKey;
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    imgPlaceholder: 'http://placekitten.com/g/80/80',
+    restaurants: []
+  },
+  created: function () {
+	this.getImage();
+  },
+  methods: {
+  	getImage: function () {
+		axios.get(urlRestaurants)
+			.then(function(res) {
+				console.clear();
+				// vm.asteroids = res.data.near_earth_objects.slice(0, 100);
+				console.log(res.data.records);
+				app.restaurants = res.data.records;
+			})
+	}
+  }
+});
